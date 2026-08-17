cdf68045abb97e4c5329f921022dc436bb2fb3bf
dappusreeja00 <dappusreeja2314@gmail.com>
2026-08-17 11:31:04 +0530

    Add files via upload

---
+++ static/js/scheme navbar.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const menuToggle = document.getElementById("menu-toggle");
+    const navMenu = document.getElementById("nav-menu");
+
+    if(menuToggle && navMenu){
+
+        menuToggle.addEventListener("click", function(){
+
+            navMenu.classList.toggle("active");
+
+        });
+
+    }
+
+});
